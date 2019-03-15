fe8f0abbf45d4125187c4d8d690697a21185b975
robrides <techjunkey@gmail.com>
2019-03-15 17:00:22 -0600

    Added User Story #2

--- src/MakeChangeProgram.java
+++ src/MakeChangeProgram.java
@@ -8,8 +8,15 @@ public class MakeChangeProgram {
 		
 		Scanner kb = new Scanner(System.in);
 
-		System.out.print("Welcome to the Make Change Program!\nEnter the price of your item >>");
+		System.out.print("Welcome to the Make Change Program!\nEnter the price of your item >> ");
 		double productPrice = kb.nextDouble();
+		
+//		User Story #2
+//		The user is then prompted asking how much money was tendered by the customer.
+
+		System.out.print("Enter the amount of money you are tendering >> ");
+		double amountTendered = kb.nextDouble();
+
 		
 	}
 
