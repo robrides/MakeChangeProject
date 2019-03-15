8ae26156aab4649b8d905f12ade7fa0064105105
robrides <techjunkey@gmail.com>
2019-03-15 16:57:22 -0600

    Added User Story #1

--- src/MakeChangeProgram.java
+++ src/MakeChangeProgram.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class MakeChangeProgram {
 
 	public static void main(String[] args) {
+//		User Story #1
+//		The user is prompted asking for the price of the item.
+		
 		Scanner kb = new Scanner(System.in);
 
+		System.out.print("Welcome to the Make Change Program!\nEnter the price of your item >>");
+		double productPrice = kb.nextDouble();
+		
 	}
 
 }
